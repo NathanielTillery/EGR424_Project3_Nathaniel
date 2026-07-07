f431464029f51652cdedb9858e2b50fcf3c266ef
NathanielTillery <nate.tillery@gmail.com>
2026-07-07 13:46:16 -0400

    Initial Publish

---
+++ main.c
@@ -0,0 +1,20 @@
+/* ****************************************************************
+ * Author: Nathaniel C. Tillery
+ * Class: EGR424 Project 3
+ * Description: MSP432 pre-emptive multi-threaded Kernel
+ * Date: 2, July 2026
+ *
+ * ****************************************************************/
+
+
+
+#include "msp.h"
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+}
